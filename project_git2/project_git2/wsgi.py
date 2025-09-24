86129a24e39f97364193af43ab9674946da742e3
AksilDev <axcelsonmacansantos@gmail.com>
2025-09-24 21:05:48 +0800

    setup(project): create the project

---
+++ project_git2/project_git2/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for project_git2 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_git2.settings')
+
+application = get_wsgi_application()
